be14607a4f1ecfe0335a67ae1601ccad441d143d
Madhusankha K.A.D <it25102645@my.sliit.lk>
2026-05-10 09:04:55 +0530

    Setup Spring Boot project structure with dependencies

---
+++ src/main/java/com/sliit/project/bikemanagement/BikemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.project.bikemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikemanagementApplication.class, args);
+	}
+
+}
